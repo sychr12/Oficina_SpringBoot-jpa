69e14ef6e2d78562126e7922091b1c4f8b946f6c
Luiz Felipe <ssychr.yr@gmail.com>
2025-10-26 19:07:25 -0400

    Product

---
+++ src/main/java/com/testebetaweb/CursoSpring/repositories/ProductRepository.java
@@ -0,0 +1,14 @@
+package com.testebetaweb.CursoSpring.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.testebetaweb.CursoSpring.entities.Product;
+
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
+
+
---
+++ src/main/java/com/testebetaweb/CursoSpring/resource/ProductResource.java
@@ -0,0 +1,35 @@
+package com.testebetaweb.CursoSpring.resource;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.testebetaweb.CursoSpring.entities.Product;
+import com.testebetaweb.CursoSpring.services.ProductService;
+
+@RestController
+@RequestMapping(value = "/Products")
+public class ProductResource {
+	
+	@Autowired
+	private ProductService service;
+	
+	@GetMapping
+	public ResponseEntity<List<Product>> findAll(){
+		List<Product> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Product>findByid(@PathVariable Long id){
+		Product obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
+}
+
