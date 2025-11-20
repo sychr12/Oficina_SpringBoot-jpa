6a0e403a77e38667f90a4d3b745d533bbed72b5b
Luiz Felipe <ssychr.yr@gmail.com>
2025-11-20 12:13:26 -0400

    Many-to-Many association with JoinTable

--- src/main/java/com/testebetaweb/CursoSpring/entities/Category.java
+++ src/main/java/com/testebetaweb/CursoSpring/entities/Category.java
@@ -5,56 +5,49 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
-import jakarta.persistence.Transient;
-
 
 @Entity
 @Table(name = "tb_category")
 public class Category implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
-	
-	
-	@Transient
-	private Set<Product> products = new HashSet<>();
 
+	@JsonIgnore
+	@ManyToMany(mappedBy = "categories")
+	private Set<Product> products = new HashSet<>();
 	
+
 	public Category() {
-		
 	}
 
-
 	public Category(Long id, String name) {
-		super();
 		this.id = id;
 		this.name = name;
 	}
-
 
 	public Long getId() {
 		return id;
 	}
 
-
 	public void setId(Long id) {
 		this.id = id;
 	}
 
-
 	public String getName() {
 		return name;
 	}
-
 
 	public void setName(String name) {
 		this.name = name;
@@ -69,7 +62,6 @@ public class Category implements Serializable {
 		return Objects.hash(id);
 	}
 
-
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -81,9 +73,5 @@ public class Category implements Serializable {
 		Category other = (Category) obj;
 		return Objects.equals(id, other.id);
 	}
-
-
-
-	
-	
 }
+
--- src/main/java/com/testebetaweb/CursoSpring/entities/Product.java
+++ src/main/java/com/testebetaweb/CursoSpring/entities/Product.java
@@ -9,32 +9,37 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
-import jakarta.persistence.Transient;
-
 
 @Entity
-@Table(name = "tb_category")
+@Table(name = "tb_product")
 public class Product implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
 	private String name;
 	private String description;
 	private Double price;
 	private String imgUrl;
-	
-	@Transient
-	private Set<Category> Categories = new HashSet<>();
-	
+
+	@ManyToMany
+	@JoinTable(
+		name = "tb_product_category",
+		joinColumns = @JoinColumn(name = "product_id"),
+		inverseJoinColumns = @JoinColumn(name = "category_id")
+	)
+	private Set<Category> categories = new HashSet<>();
+
 	public Product() {
-		
 	}
 
 	public Product(Long id, String name, String description, Double price, String imgUrl) {
-		super();
 		this.id = id;
 		this.name = name;
 		this.description = description;
@@ -83,7 +88,7 @@ public class Product implements Serializable {
 	}
 
 	public Set<Category> getCategories() {
-		return Categories;
+		return categories;
 	}
 
 	@Override
@@ -95,11 +100,10 @@ public class Product implements Serializable {
 	public boolean equals(Object obj) {
 		if (this == obj)
 			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
+		if (obj == null || getClass() != obj.getClass())
 			return false;
 		Product other = (Product) obj;
 		return Objects.equals(id, other.id);
 	}
 }
+
